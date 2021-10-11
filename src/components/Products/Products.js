d72074021ef390947c4d4b774ada79bae8357893
Chagit <chagit990@gmail.com>
2021-10-11 14:02:25 +0300

    Homework2

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,20 +1,29 @@
 import './Products.css';
-import Product from '../Product/Product'; 
+import Product from '../Product/Product';
+import React from 'react'; 
 
-function Products(){
+function Products({List}){
     return(
-        <section className="products">
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
-       </section> 
+         <section className="products">
+    {/* //     <Product/>
+    //     <Product/>
+    //     <Product/>
+    //     <Product/>
+    //     <Product/>
+    //     <Product/>
+    //     <Product/>
+    //     <Product/>
+    //     <Product/>
+    //     <Product/> */}
+         
+    
+        {List.map((product)=>(
+            
+                <Product  key={product.id} id={product.id} title={product.title} price={product.price}  description={product.description} category={product.category} image={product.image} rating={product.rating}/>
+            
+        ))}
+    
+    </section>
    );  
 }
 export default Products;
